6194e39ce74559bb02eb81bcdf0c98d57f134759
Brett Strydom <bstrydo@thoughtworks.com>
2017-01-26 14:45:30 +0200

    changed plugin to hook into on_before_build_all

--- packages/archiver/lektor_archiver.py
+++ packages/archiver/lektor_archiver.py
@@ -6,7 +6,5 @@ class ArchiverPlugin(Plugin):
     name = u'archiver'
     description = u'This plugin archives old articles.'
 
-    def on_process_template_context(self, context, **extra):
-        def test_function():
-            return 'Value from plugin %s' % self.name
-        context['test_function'] = test_function
+    def on_before_build_all(self, builder, **extra):
+        print 'hello world, brett is going wild'
